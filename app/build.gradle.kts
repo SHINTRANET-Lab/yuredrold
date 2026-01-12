7ebe64380076c1427a9e33d349fcfffd1c1df961
SHINANO MAI(:3) <admin@shinanomai.xyz>
2026-01-12 19:07:01 +0900

    meta version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         minSdk = 24
         // Keep targetSdk up-to-date to a modern level to get behavior and security improvements
         targetSdk = 33
-        versionCode = 7
-        versionName = "1.5.2-Linear"
+        versionCode = 8
+        versionName = "1.5.3-Linear"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
